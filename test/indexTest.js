8cd23f1ab80072f409406cb41da58f30d9cd5ae3
Cerunnos <github email address>
2018-02-22 19:45:37 -0500

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -18,7 +18,7 @@ describe('driver', function() {
 
   describe('yearsExperienceFromBeginningOf', function() {
     it('calculates the number of years driven given an endDate', function() {
-      expect(driver.yearsExperienceFromBeginningOf(2017)).to.equal(22)
+      expect(driver.yearsExperienceFromBeginningOf(2017)).to.equal(21)
     })
   })
 })
@@ -29,29 +29,59 @@ describe('Route', function() {
 
   describe('blocksTravelled', function() {
     it('calculates the number of blocksTravelled', function() {
-      let route = new Route({horizontal: 'Park', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
+      let route = new Route({
+        horizontal: 'Park',
+        vertical: '34'
+      }, {
+        horizontal: 'Park',
+        vertical: '45'
+      })
       expect(route.blocksTravelled()).to.equal(11)
     })
 
     it('calculates the number of horizontal blocks travelled', function() {
-      let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '34'})
+      let route = new Route({
+        horizontal: '1st Avenue',
+        vertical: '34'
+      }, {
+        horizontal: 'Park',
+        vertical: '34'
+      })
       expect(route.blocksTravelled()).to.equal(4)
     })
 
     it('combines horizontal and vertical blocks travelled', function() {
-      let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
+      let route = new Route({
+        horizontal: '1st Avenue',
+        vertical: '34'
+      }, {
+        horizontal: 'Park',
+        vertical: '45'
+      })
       expect(route.blocksTravelled()).to.equal(15)
     })
   })
 
   describe('estimatedTime', function() {
     it('estimates time in minutes, with travel time of three blocks in a minute', function() {
-      let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
+      let route = new Route({
+        horizontal: '1st Avenue',
+        vertical: '34'
+      }, {
+        horizontal: 'Park',
+        vertical: '45'
+      })
       expect(route.estimatedTime()).to.equal(5)
     })
 
     it('estimates time in minutes, with travel time of two blocks in a minute during peak hours', function() {
-      let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '46'})
+      let route = new Route({
+        horizontal: '1st Avenue',
+        vertical: '34'
+      }, {
+        horizontal: 'Park',
+        vertical: '46'
+      })
       expect(route.estimatedTime(true)).to.equal(8)
     })
   })
